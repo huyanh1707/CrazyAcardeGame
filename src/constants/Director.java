be8627773afc795882780edbb613ebd5840741cd
Huy Anh <ndhuyanh1707@gmail.com>
2021-11-26 00:29:22 +0700

    add key input controller for character

---
+++ src/constants/Director.java
@@ -0,0 +1,6 @@
+package constants;
+
+public enum Director {
+    UP, DOWN, LEFT, RIGHT;
+    public static Director[] dir = {UP, DOWN, LEFT, RIGHT};
+}
---
+++ src/gamelogic/KeyInput.java
@@ -0,0 +1,42 @@
+package gamelogic;
+
+import constants.Director;
+import entities.Player;
+import javafx.scene.input.KeyCode;
+
+import java.util.List;
+
+public class KeyInput {
+
+    public void playerKeyHandler() {
+        List<KeyCode> keyBoardInputs = KeyController.getPlayerController();
+        Player player = Player.getPlayer();
+
+        if (keyBoardInputs.contains(KeyCode.UP) || keyBoardInputs.contains(KeyCode.W)) {
+            player.move(player.getSpeed(), Director.UP);
+        }
+
+        if (keyBoardInputs.contains(KeyCode.DOWN) || keyBoardInputs.contains(KeyCode.S)) {
+            player.move(player.getSpeed(), Director.DOWN);
+        }
+
+        if (keyBoardInputs.contains(KeyCode.LEFT) || keyBoardInputs.contains(KeyCode.A)) {
+            player.move(player.getSpeed(), Director.LEFT);
+        }
+
+        if (keyBoardInputs.contains(KeyCode.RIGHT) || keyBoardInputs.contains(KeyCode.D)) {
+            player.move(player.getSpeed(), Director.RIGHT);
+        }
+
+        if (!keyBoardInputs.contains(KeyCode.UP) && !keyBoardInputs.contains(KeyCode.W)
+                && !keyBoardInputs.contains(KeyCode.DOWN) && !keyBoardInputs.contains(KeyCode.S)
+                && !keyBoardInputs.contains(KeyCode.LEFT) && !keyBoardInputs.contains(KeyCode.A)
+                && !keyBoardInputs.contains(KeyCode.RIGHT) && !keyBoardInputs.contains(KeyCode.D)) {
+            player.move(0, Director.DOWN);
+        }
+
+//        if (keyBoardInputs.contains(KeyCode.SPACE)) {
+//            player.placeBomb();
+//        }
+    }
+}
